aacea3835d3cea7934d6e297f2e256c3c444cd8d
Hwa Hui <nws04_01058695@pb.edu.bn>
2018-02-27 23:21:21 +0800

    login page 80% update

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { User } from '../../models/user';
 import { AngularFireAuth } from "angularfire2/auth";
-import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators, AbstractControl } from "@angular/forms";
 
 @IonicPage({
   name: 'LoginPage'
@@ -13,6 +13,7 @@ import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from
   templateUrl: 'login.html',
   providers: [AngularFireAuth]
 })
+
 export class LoginPage {
 
   user = {} as User;
@@ -22,13 +23,14 @@ export class LoginPage {
   password:AbstractControl;
 
 
-  constructor(private afAuth: AngularFireAuth, 
+  constructor(public alertCtrl: AlertController,
+    private afAuth: AngularFireAuth, 
     public loadingCtrl: LoadingController,
     public navCtrl: NavController, public navParams: NavParams,
     public formbuilder: FormBuilder) {
 
       this.formgroup = formbuilder.group({
-        email:['',[Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._]+[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')]],
+        email:['',[Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
         password:['',[Validators.required, Validators.minLength(6)]]
       });
       
@@ -36,26 +38,52 @@ export class LoginPage {
       this.password = this.formgroup.controls['password'];
   }
 
+  // Go To Register Page
   navigateToRegisterPage(){
     this.navCtrl.push('RegisterPage');
   }
 
+  // Go To Lost Password Page
   navigateToLostPasswordPage(){
     this.navCtrl.push('LostPasswordPage');
   }
 
+  // when login button clicked
   async login(user: User) {
-    try {
-      const result = this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
-      let loader = this.loadingCtrl.create({
+    // create loading 
+    let loader = this.loadingCtrl.create({
+    })
+    loader.present();
+    // check whether email and password are valid
+    this.afAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+    .then(auth => {
+      // if valid; set HomePage as root page
+      this.navCtrl.setRoot('HomePage');
+      loader.dismiss();
+    })
+    .catch(err => {
+      // if not valid; create alert message
+      let alert = this.alertCtrl.create({
+        title: 'Invalid',
+        subTitle: 'Incorrect Email / Password <br\> Please Try Again!',
+        buttons: ['OK']
       });
-      loader.present();
-      if (result) {
-        this.navCtrl.push('HomePage');
-        loader.dismiss();
-      } 
-    } catch (e) {
-      console.error(e);
-    }
+      // loading disapear & alert appear
+      loader.dismiss();
+      alert.present();
+    }); 
   }
 }
+
+// try {
+    //   const result = this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+    //   let loader = this.loadingCtrl.create({
+    //   });
+    //   loader.present();
+    //   if (result) {
+    //     loader.dismiss();
+    //     this.navCtrl.push('HomePage');
+    //   }
+    // } catch (e) {
+    //   console.error(e);
+    // }
