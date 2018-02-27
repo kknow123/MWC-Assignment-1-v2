cd2c8d810b5857368ba5789333ff4083aaa504c0
Hwa Hui <nws04_01058695@pb.edu.bn>
2018-02-27 20:56:35 +0800

    email validation pattern

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { User } from '../../models/user';
 import { AngularFireAuth } from "angularfire2/auth";
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from "@angular/forms";
 
 @IonicPage({
   name: 'LoginPage'
 })
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
@@ -15,9 +17,23 @@ export class LoginPage {
 
   user = {} as User;
 
+  formgroup:FormGroup;
+  email:AbstractControl;
+  password:AbstractControl;
+
+
   constructor(private afAuth: AngularFireAuth, 
-    
-    public navCtrl: NavController, public navParams: NavParams) {
+    public loadingCtrl: LoadingController,
+    public navCtrl: NavController, public navParams: NavParams,
+    public formbuilder: FormBuilder) {
+
+      this.formgroup = formbuilder.group({
+        email:['',[Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._]+[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')]],
+        password:['',[Validators.required, Validators.minLength(6)]]
+      });
+      
+      this.email = this.formgroup.controls['email'];
+      this.password = this.formgroup.controls['password'];
   }
 
   navigateToRegisterPage(){
@@ -31,17 +47,15 @@ export class LoginPage {
   async login(user: User) {
     try {
       const result = this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+      let loader = this.loadingCtrl.create({
+      });
+      loader.present();
       if (result) {
         this.navCtrl.push('HomePage');
+        loader.dismiss();
       } 
     } catch (e) {
       console.error(e);
     }
-   
   }
-
-  // login() {
-  //   this.navCtrl.push('HomePage');
-  // }
-
 }
