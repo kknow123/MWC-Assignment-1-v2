351c784a895d5dd327d43558b16a7f623bea5d69
Hwa Hui <nws04_01058695@pb.edu.bn>
2018-02-27 12:12:51 +0800

    install firebase

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG =  {
+    apiKey: "AIzaSyAcCaaTbzZosL9hMfoakK1MOwZACEZffW0",
+    authDomain: "bos-firebase.firebaseapp.com",
+    databaseURL: "https://bos-firebase.firebaseio.com",
+    projectId: "bos-firebase",
+    storageBucket: "bos-firebase.appspot.com",
+    messagingSenderId: "856277956007"
+  };
