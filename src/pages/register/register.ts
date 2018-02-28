43b6333f2daf047cc5d677299f034d7799fe51ea
Hwa Hui <nws04_01058695@pb.edu.bn>
2018-02-28 14:58:40 +0800

    register validation alert updates

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { User } from '../../models/user';
 import { AngularFireAuth } from "angularfire2/auth";
+import { FormBuilder, FormGroup, Validators, AbstractControl } from "@angular/forms";
 
 @IonicPage({
   name: 'RegisterPage'
@@ -16,9 +17,27 @@ export class RegisterPage {
 
   user = {} as User;
 
-  constructor(private afAuth: AngularFireAuth,
-    
-    public navCtrl: NavController, public navParams: NavParams) {
+  formgroup:FormGroup;
+  email:AbstractControl;
+  password:AbstractControl;
+  confirmPassword: AbstractControl;
+
+  constructor(
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    private afAuth: AngularFireAuth,
+    public navCtrl: NavController, public navParams: NavParams,
+    public formbuilder: FormBuilder) {
+
+      this.formgroup = formbuilder.group({
+        email:['',[Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+        password:['',[Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
+        confirmPassword:['',[Validators.required, Validators.minLength(6), Validators.maxLength(12)]]
+      });
+
+      this.email = this.formgroup.controls['email'];
+      this.password = this.formgroup.controls['password'];
+      this.confirmPassword = this.formgroup.controls['confirmPassword'];
   }
 
   navigateToLoginPage() {
@@ -26,12 +45,38 @@ export class RegisterPage {
   }
 
   async register(user: User) {
-    try {
-      const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
-      console.log(result);
-    }
-    catch(e) {
-      console.error(e);
+    {
+      // create loading 
+      let loader = this.loadingCtrl.create({
+      })
+      loader.present();
+      // check whether email are in use
+      this.afAuth.auth.createUserWithEmailAndPassword(this.user.email, this.user.password)
+      .then(auth => {
+        // if not in use; go back to LoginPage
+        let alert = this.alertCtrl.create({
+          subTitle: 'Successfully Registered!',
+          buttons: [{
+            text: 'OK',
+             handler: data => {
+              this.navCtrl.push('LoginPage');
+              loader.dismiss();
+            }
+          }]
+        });
+        alert.present();
+      })
+      .catch(err => {
+        // if it is already in use; create alert message
+        let alert = this.alertCtrl.create({
+          title: 'Invalid',
+          subTitle: 'Email already in-use<br\> Please Try Again!',
+          buttons: ['OK']
+        });
+        // loading disapear & alert appear
+        loader.dismiss();
+        alert.present();
+      }); 
     }
   }
 }
