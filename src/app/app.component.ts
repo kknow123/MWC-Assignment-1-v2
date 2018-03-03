5ecc477b7672e5483d6e61420a594fa38f1038fc
JuliyanDamak <nws04_01105398@pb.edu.bn>
2018-03-03 09:36:56 +0800

    Tabs Below

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import {TabsPage} from '../pages/tabs/tabs';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:string = 'SliderPage';
+  rootPage:any = 'TabsPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the TabsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+  tab1 = 'HomePage';
+  tab2 = 'CategoriesPage';
+  tab3 = 'CartPage';
+  tab4 = 'AccountPage';
+  tab5 = 'SliderPage';
+  tab6 = 'LoginPage';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabsPage');
+  }
+
+}
