29631330746323c6601b47d83a4ff2a51c3416d6
Hwa Hui <nws04_01058695@pb.edu.bn>
2018-03-06 02:29:01 +0800

    categories changes

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,12 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ItemPage } from '../item/item';
-/**
- * Generated class for the CategoriesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,18 +8,37 @@ import { ItemPage } from '../item/item';
 })
 export class CategoriesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    
-  }
-  navigateToItemPage(){
-    this.navCtrl.push(ItemPage);
-  }
+  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
+
+  SwipedTabsIndicator :any= null;
+  tabs:any=[];
+
  
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriesPage');
+  constructor(public navCtrl: NavController) {
+  	this.tabs=["Fashion","Computer","Book"];
   }
 
-  
+  ionViewDidEnter() {
+    this.SwipedTabsIndicator = document.getElementById("indicator");
+  }
+
+  selectTab(index) {    
+    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+    this.SwipedTabsSlider.slideTo(index, 500);
+  }
+
+  updateIndicatorPosition() {
+      // this condition is to avoid passing to incorrect index
+  	if (this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex()){
+  		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
+  	}
+    
+    }
+
+  animateIndicator($event) {
+  	if(this.SwipedTabsIndicator)
+   	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
+  }
+
 
 }
